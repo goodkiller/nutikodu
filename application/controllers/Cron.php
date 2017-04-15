3ad6a639dd1dac5347ec9e07c89ff0565c1cdfa6
Marko Praakli <markopraakli@gmail.com>
2017-04-15 10:43:42 +0300

    Cron locking

--- application/controllers/Cron.php
+++ application/controllers/Cron.php
@@ -5,12 +5,62 @@ class Cron extends CI_Controller {
 
 	public function jobs()
 	{
-		log_message( 'debug', '[CRON] Start jobs.' );
+		log_message( 'debug', '[CRON] Start cron.' );
 	
-		$this->load->library( 'Jobs' );
+		// Check status
+		if( $this->get_status() === 0 )
+		{
+			// Lock cron
+			$this->lock();
 
-		$this->jobs->check_all();
+			$this->load->library( 'Jobs' );
 
-		log_message( 'debug', '[CRON] End jobs.' );
+			$this->jobs->check_all();
+
+			// Unclock cron
+			$this->unlock();
+
+			log_message( 'debug', '[CRON] End cron.' );
+		}
+		else
+		{
+			log_message( 'debug', '[CRON] End cron: locked!' );
+		}
+	}
+
+	private function get_status()
+	{
+		$lock_status = (int)$this->db->get_where( 'settings', array( 'key' => 'cron_lock' ) )->row( 'val' );
+
+		return $lock_status;
+	}
+
+	private function lock()
+	{
+		// Add lock
+		$this->db->set( 'val', 1 );
+		$this->db->where( 'key', 'cron_lock' );
+		$this->db->update( 'settings' );
+
+		log_message( 'debug', '[CRON] Lock cron.' );
+
+		return TRUE;
+	}
+
+	private function unlock()
+	{
+		// Remove lock
+		$this->db->set( 'val', 0 );
+		$this->db->where( 'key', 'cron_lock' );
+		$this->db->update( 'settings' );
+
+		// Update last run date
+		$this->db->set( 'val', 'NOW()', FALSE );
+		$this->db->where( 'key', 'cron_last_run' );
+		$this->db->update( 'settings' );
+
+		log_message( 'debug', '[CRON] Unlock cron.' );
+
+		return TRUE;
 	}
 }
